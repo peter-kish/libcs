2d790047c7281d2265406c4b376cdde632a054e4
Peter Kish <pkish023@gmail.com>
2016-11-05 10:22:13 +0100

    Added invalid IDs.

--- idfactory.h
+++ idfactory.h
@@ -6,11 +6,12 @@
 namespace cs
 {
     typedef unsigned int ID;
+    const ID INVALID_ID = 0;
 
     class IDFactory
     {
         public:
-            IDFactory() : lastID(0) {};
+            IDFactory() : lastID(1) {};
             ID getNewID();
             static unsigned long getMaxNoOfIDs();
         private:
